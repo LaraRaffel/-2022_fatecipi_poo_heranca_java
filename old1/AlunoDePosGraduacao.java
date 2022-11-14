88a4013157b227694066828d0ff56b1f7d87d5e3
Lara Raffel <lara.b.r@outlook.com>
2022-11-14 15:58:44 -0300

    heranca(apostila05): fim da listagem 1.2.4

---
+++ old1/AlunoDePosGraduacao.java
@@ -0,0 +1,5 @@
+public class AlunoDePosGraduacao {
+    private String nome;
+    private int idade;
+    private char conceito;
+}
---
+++ old1/ProfessorHorista.java
@@ -0,0 +1,4 @@
+public class ProfessorHorista {
+    private String nome;
+    private int idade;
+}
